0945eaeae8660acefea84f20946f790faf99ea0c
admin <admin>
2020-07-29 10:32:29 +0200

    Edited file 'python-lib/design/helpers.py'

---
+++ python-lib/design/helpers.py
@@ -0,0 +1,14 @@
+from io import StringIO
+from dataiku.core.dkujson import dumps
+import time
+
+
+1+1
+def save_parameters(variables, folder):
+    fields = [u'ratio', u'power', u'mde', u'tail', u'traffic', u'n_B', u'sig_level', u'n_A', u'bcr']
+    tracking = {key: variables[key] for key in variables if key in fields}
+    filename = str(time.time())
+    parameters = StringIO()
+    parameters.write(dumps(tracking))
+    parameters.seek(0)
+    folder.upload_stream(filename, parameters)
