0303af9fdb190ceb9bdf202cde1e8f7b93e8c2a5
Marine <marine@dku-mbp-marine.home>
2020-08-03 13:48:42 +0200

    managed the ugly UI error

--- resource/py/compute_files_in_folder.py
+++ resource/py/compute_files_in_folder.py
@@ -2,12 +2,16 @@ from dataiku import Folder
 
 
 def do(payload, config, plugin_config, inputs):
+    folder = None
     for recipe_input in inputs:
         if recipe_input["role"] == "folder":
             folder = Folder(recipe_input["fullName"])
-    paths = folder.list_paths_in_partition()
-    choices = []
-    for file_name in paths:
-        if ".json" in file_name:
-            choices.append({"value": file_name, "label": file_name})
-    return {"choices": choices}
+    if folder:
+        paths = folder.list_paths_in_partition()
+        choices = []
+        for file_name in paths:
+            if ".json" in file_name:
+                choices.append({"value": file_name, "label": file_name})
+        return {"choices": choices}
+    else:
+        return {"choices": [{"value": None, "label": "Invalid : no input folder"}]}
