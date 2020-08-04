512aa2cf075cb65d414dfff35474db322e99bf23
Marine <marine@dku-mbp-marine.home>
2020-08-04 14:49:25 +0200

    added logger

--- python-lib/design/ab_dispatcher.py
+++ python-lib/design/ab_dispatcher.py
@@ -1,5 +1,9 @@
 import numpy as np
 import pandas as pd
+import logging
+
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO, format="AB testing plugin %(levelname)s - %(message)s")
 
 
 class AbDispatcher(object):
@@ -13,13 +17,23 @@ class AbDispatcher(object):
         self.check_sample_size(experiment_population)
         A_group, B_group = self.split_into_groups(experiment_population, attribution_method)
         group_df = self.add_group_column(input_df, A_group, B_group)
+        logger.info("Dispatching experiment population: done")
         return group_df, A_group, B_group
 
     def check_sample_size(self, experiment_population: np.array):
-        if self.size_A + self.size_B > experiment_population.shape[0]:
+        population_size = experiment_population.shape[0]
+
+        logger.info("Dispatching experiment population ...")
+        logger.info("Dispatching experiment population: checking the sample sizes")
+        logger.info("Sample size of the A group:" + str(self.size_A))
+        logger.info("Sample size of the B group:" + str(self.size_B))
+        logger.info("Size of the experiment population: " + str(population_size))
+
+        if self.size_A + self.size_B > population_size:
             raise ValueError("Not enough user ids or emails to run the current AB testing.")
 
     def split_into_groups(self, experiment_population: np.array, attribution_method: str) -> tuple:
+        logger.info("Dispatching experiment population: shuffle")
         np.random.permutation(experiment_population)
         if attribution_method == "leftover_to_A":
             B_group = experiment_population[:self.size_B]
