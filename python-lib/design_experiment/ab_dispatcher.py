71c913edcddf909a14b0b1f80d9fadee2478b515
Marine <marine@DKU-MBP-marine.local>
2020-08-17 15:22:27 +0100

    replaced the name attribution with leftovers_management

--- python-lib/design_experiment/ab_dispatcher.py
+++ python-lib/design_experiment/ab_dispatcher.py
@@ -15,11 +15,11 @@ class AbDispatcher(object):
         self.size_A = size_A
         self.size_B = size_B
 
-    def dispatch(self, input_df: pd.DataFrame, reference_column: str, attribution: AttributionMethod) -> tuple:
+    def dispatch(self, input_df: pd.DataFrame, reference_column: str, leftovers_management: AttributionMethod) -> tuple:
         """Dispatch the experiment population into two groups
 
         :param pd.DataFrame input_df: population dataset, an input of the recipe
-        :param AttributionMethod attribution: defines how to deal with the leftover users.
+        :param AttributionMethod leftovers_management: defines how to deal with the leftover users.
         :raises: :class:`ValueError`: Not enough users to run the experiment
 
         :returns: Experiment dataset and two arrays containing the contacts for each group
@@ -27,7 +27,7 @@ class AbDispatcher(object):
         """
         population_df = input_df.drop_duplicates(subset=[reference_column])
         self.check_sample_size(population_df)
-        group_df = self.split_into_groups(population_df, attribution)
+        group_df = self.split_into_groups(population_df, leftovers_management)
         return group_df
 
     def check_sample_size(self, df_unique_ids: pd.DataFrame):
@@ -46,7 +46,7 @@ class AbDispatcher(object):
         if self.size_A + self.size_B > population_size:
             raise ValueError("Not enough user ids or emails to run the current AB testing.")
 
-    def split_into_groups(self, population_df: pd.DataFrame, attribution_method: AttributionMethod) -> tuple:
+    def split_into_groups(self, population_df: pd.DataFrame, leftovers_management: AttributionMethod) -> tuple:
         """Shuffle the population and splits it into groups
 
         :returns: Two arrays containing the contacts for each group
@@ -55,17 +55,17 @@ class AbDispatcher(object):
         logger.info("Dispatching experiment population: shuffle")
         random_generator = Generator(np.random.PCG64())
         index = population_df.index
-        if attribution_method == AttributionMethod.LEFTOVER_TO_A:
+        if leftovers_management == AttributionMethod.LEFTOVER_TO_A:
             population_df[Column.AB_GROUP.value] = Group.A.value
             sampled_index = random_generator.choice(index, self.size_B, replace=False)
             column_index = population_df.columns.get_loc(Column.AB_GROUP.value)
             population_df.iloc[sampled_index, column_index] = Group.B.value
-        elif attribution_method == AttributionMethod.LEFTOVER_TO_B:
+        elif leftovers_management == AttributionMethod.LEFTOVER_TO_B:
             population_df[Column.AB_GROUP.value] = Group.B.value
             sampled_index = random_generator.choice(index, self.size_A, replace=False)
             column_index = population_df.columns.get_loc(Column.AB_GROUP.value)
             population_df.iloc[sampled_index, column_index] = Group.A.value
-        elif attribution_method == AttributionMethod.LEFTOVER_BLANK:
+        elif leftovers_management == AttributionMethod.LEFTOVER_BLANK:
             population_df[Column.AB_GROUP.value] = np.nan
             sampled_index = random_generator.choice(index, self.size_A + self.size_B, replace=False)
             column_index = population_df.columns.get_loc(Column.AB_GROUP.value)
