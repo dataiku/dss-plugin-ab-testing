bba82ba46bff3a81fde590d84fda88e599b129da
Marine <marine@DKU-MBP-marine.local>
2020-09-09 17:26:47 +0100

    axes update in a smoother way

--- resource/js/visualisation/chart.js
+++ resource/js/visualisation/chart.js
@@ -1,5 +1,11 @@
 function plot_chart($scope) {
+    let y_max = get_ymax($scope);
+    let x_max = get_xmax($scope.distribution_B);
+    let suggested_x_max = Math.ceil(x_max / 0.05) * 0.05
+    suggested_x_max = suggested_x_max.toFixed(2);
+
     Chart.defaults.scale.gridLines.drawOnChartArea = false;
+
     chart = new Chart(document.getElementById("chart"), {
         type: 'line',
         data: {
@@ -16,7 +22,16 @@ function plot_chart($scope) {
             },
             scales: {
                 xAxes: [{
-                    type: 'linear'
+                    type: 'linear',
+                    ticks: {
+                        suggestedMin: -suggested_x_max + $scope.bcr / 100,
+                        suggestedMax: suggested_x_max
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        suggestedMax: Math.ceil(y_max / 10) * 10
+                    }
                 }]
             }
         }
@@ -25,36 +40,38 @@ function plot_chart($scope) {
 }
 
 function update_chart($scope) {
+    let mde = $scope.mde / 100;
+    let bcr = $scope.bcr / 100;
+    $scope.std = Math.sqrt(bcr * (1 - bcr) * (1 / $scope.sample_size_A + 1 / $scope.sample_size_B) * 100 / $scope.reach);
+    $scope.distribution_A = Random_normal_Dist(0, $scope.std);
+    $scope.distribution_B = Random_normal_Dist(mde, $scope.std);
+
     $scope.chart.config.data.datasets = get_datasets($scope);
+    update_axes_bound($scope);
     $scope.chart.update(0);
 }
 
-function erase_chart($scope){
-    $scope.chart.config.data.datasets =[];
+function erase_chart($scope) {
+    $scope.chart.config.data.datasets = [];
     $scope.chart.update(0);
 }
 
 function get_datasets($scope) {
-    let mde = $scope.mde / 100;
     let sig_level = $scope.sig_level / 100;
-    let bcr = $scope.bcr / 100;
-    let std = Math.sqrt(bcr * (1 - bcr) * (1 / $scope.sample_size_A + 1 / $scope.sample_size_B) * 100 / $scope.reach);
-    let distribution_A = Random_normal_Dist(0, std);
-    let distribution_B = Random_normal_Dist(mde, std);
-    let z_value = compute_z_value(std, 1 - sig_level, $scope.tail);
+    let z_value = compute_z_value($scope.std, 1 - sig_level, $scope.tail);
 
-    let CI_line = get_CI(distribution_A, distribution_B, z_value)
-    let area_boundary_A = draw_area(distribution_A, z_value, 0, std);
-    let area_boundary_B = draw_area(distribution_B, z_value, mde, std);
+    let CI_line = get_CI($scope, z_value);
+    let area_boundary_A = draw_area($scope.distribution_A, z_value, 0, $scope.std);
+    let area_boundary_B = draw_area($scope.distribution_B, z_value, $scope.mde / 100, $scope.std);
 
     chart_datasets = [{
-        data: distribution_A,
+        data: $scope.distribution_A,
         borderColor: "rgba(47, 53, 66,1.0)",
         fill: false,
         label: "H0"
     },
     {
-        data: distribution_B,
+        data: $scope.distribution_B,
         borderColor: "#ffc845",
         fill: false,
         label: "H1"
@@ -84,8 +101,17 @@ function get_datasets($scope) {
     return chart_datasets
 }
 
-function get_CI(distribution_A, distribution_B, z_value) {
-    let y_max = get_ymax(distribution_A, distribution_B);
+function update_axes_bound($scope) {
+    let y_max = get_ymax($scope);
+    $scope.chart.options.scales.yAxes[0].ticks = { suggestedMax: Math.ceil(y_max / 10) * 10 };
+    let x_max = get_xmax($scope.distribution_B);
+    let suggested_max = Math.ceil(x_max / 0.05) * 0.05;
+    suggested_max = suggested_max.toFixed(2);
+    $scope.chart.options.scales.xAxes[0].ticks = { suggestedMax: suggested_max, suggestedMin: -suggested_max + $scope.mde / 100 };
+}
+
+function get_CI($scope, z_value) {
+    let y_max = get_ymax($scope);
     let CI_line = [
         {
             x: z_value,
@@ -100,7 +126,7 @@ function get_CI(distribution_A, distribution_B, z_value) {
 }
 
 function draw_area(distribution, z_value, mean, std) {
-    let x_max = get_x_max(distribution);
+    let x_max = get_xmax(distribution);
     var line = new Array();
     for (var i = x_max; i > z_value; i -= 1 / 5000) {
         line.push({
