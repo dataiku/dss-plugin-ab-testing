8e1b6245c1013afb26d74372fe3c99cdb31779fb
Marsobad <marine.sobas@dataiku.com>
2021-02-24 10:35:35 +0100

    replace html values with ng scope to update duration

--- resource/js/form.js
+++ resource/js/form.js
@@ -1,24 +1,24 @@
 // compute duration
 function manage_duration($scope) {
     if ($scope.traffic != undefined) {
-        display_experiment_duration($scope.hide_duration);
+        display_experiment_duration($scope);
     } else {
         hide_field("duration");
     };
 }
 
 
-function display_experiment_duration() {
-    let duration = compute_duration();
+function display_experiment_duration($scope) {
+    let duration = compute_duration($scope);
     $("#nb_of_days").html(duration);
     $("#duration").removeClass('d-none');
 }
 
 
-function compute_duration() {
+function compute_duration($scope) {
     let daily_traffic = parseFloat($("#traffic").val());
-    let n_A = parseFloat($("#sample_size_A").html());
-    let n_B = parseFloat($("#sample_size_B").html());
+    let n_A = $scope.sample_size_A;
+    let n_B = $scope.sample_size_B;
     let sample_size = n_A + n_B;
     return Math.ceil(sample_size / daily_traffic);
 }
